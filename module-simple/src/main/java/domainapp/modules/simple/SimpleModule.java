620f3e6a8ae485933b5990e8e167a73bdbce0b54
danhaywood <dan@haywood-associates.co.uk>
2021-03-10 06:44:02 +0000

    splits out tests into their own modules (not yet building)

--- module-simple/src/main/java/domainapp/modules/simple/SimpleModule.java
+++ module-simple/src/main/java/domainapp/modules/simple/SimpleModule.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.validation.annotation.Validated;
 
@@ -14,11 +15,10 @@ import lombok.Data;
 
 import domainapp.modules.simple.dom.so.SimpleObject;
 
-@org.springframework.context.annotation.Configuration
-//@Import({})
+@Configuration
 @ComponentScan
 @EnableJpaRepositories
-@EntityScan(basePackageClasses = {SimpleObject.class})
+@EntityScan(basePackageClasses = {SimpleModule.class})
 @EnableConfigurationProperties({
         SimpleModule.Configuration.class,
 })
