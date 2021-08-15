f848c6dc40d8f9540f6e185adb5c0e4ff65f56ab
danhaywood <dan@haywood-associates.co.uk>
2021-08-15 17:26:29 +0100

    fixes home page logicalTypeName; removes secman menu items (added accidentally previously); force h2 to use MSSQLServer compatible mode.

--- webapp/src/main/java/domainapp/webapp/application/services/homepage/HomePageViewModel.java
+++ webapp/src/main/java/domainapp/webapp/application/services/homepage/HomePageViewModel.java
@@ -8,14 +8,13 @@ import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.DomainObjectLayout;
 import org.apache.isis.applib.annotation.HomePage;
 import org.apache.isis.applib.annotation.Nature;
-import org.apache.isis.applib.services.i18n.TranslatableString;
 
 import domainapp.modules.simple.dom.so.SimpleObject;
 import domainapp.modules.simple.dom.so.SimpleObjects;
 
 @DomainObject(
         nature = Nature.VIEW_MODEL,
-        objectType = "domainapp.HomePageViewModel"
+        logicalTypeName = "simple.HomePageViewModel"
         )
 @HomePage
 @DomainObjectLayout()
