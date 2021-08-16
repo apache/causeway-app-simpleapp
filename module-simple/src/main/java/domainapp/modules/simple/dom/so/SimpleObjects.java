e96e9fc2a71d157da651b0c701f27c90b5f91ba9
danhaywood <dan@haywood-associates.co.uk>
2021-08-16 06:50:16 +0100

    removes double annotation; fixes logger definitions

--- module-simple/src/main/java/domainapp/modules/simple/dom/so/SimpleObjects.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/so/SimpleObjects.java
@@ -25,7 +25,6 @@ import domainapp.modules.simple.types.Name;
         logicalTypeName = "simple.SimpleObjects"
 )
 @javax.annotation.Priority(PriorityPrecedence.EARLY)
-@javax.annotation.Priority(PriorityPrecedence.EARLY)
 @lombok.RequiredArgsConstructor(onConstructor_ = {@Inject} )
 public class SimpleObjects {
 
